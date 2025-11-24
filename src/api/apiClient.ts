b2a51c41092ed5c2b5c06fc94ef2e344469df6dd
Alfred Brännare <alfred-brannare@hotmail.com>
2025-11-24 15:07:10 +0100

    chore: implement refresh logic with axios interceptor and promises

--- src/api/apiClient.ts
+++ src/api/apiClient.ts
@@ -8,17 +8,62 @@ const apiClient = axios.create({
     }
 });
 
-export default apiClient;
+let isRefreshing = false;
+let failedQueue: any[] = [];
+
+const processQueue = (error: any, token: string | null = null) => {
+    failedQueue.forEach(prom => {
+        if (error) {
+            prom.reject(error);
+        } else {
+            prom.resolve(token);
+        }
+    });
+    failedQueue = [];
+}
 
 apiClient.interceptors.response.use(
     (response) => response,
     async (error) => {
+        const originalRequest = error.config;
         const status = error.response?.status;
 
-        if (status === 401 || status === 403) {
-            console.log("Authentication failed. Forcing logout.")
+        if (status === 401 && !originalRequest._retry) {
+            if (isRefreshing) {
+                return new Promise((resolve, reject) => {
+                    failedQueue.push({resolve, reject});
+                }).then(() => {
+                    return apiClient(originalRequest);
+                }).catch((error) => {
+                    return Promise.reject(error);
+                });
+            }
+
+            originalRequest._retry = true;
+            isRefreshing = true;
+
+            try {
+                await apiClient.post('auth/refresh');
+
+                processQueue(null);
+                isRefreshing = false;
+
+                return apiClient(originalRequest);
+            } catch (refreshError) {
+                processQueue(refreshError, null);
+                isRefreshing = false;
+
+                window.location.href = '/';
+                return Promise.reject(refreshError);
+            }
+        }
+
+        if (status === 403) {
+            console.log('Forbidden');
         }
 
         return Promise.reject(error);
     }
 )
+
+export default apiClient;
