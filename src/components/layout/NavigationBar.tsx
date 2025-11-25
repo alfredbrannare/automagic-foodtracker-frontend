75d78b75f73ffc66034bc55d6c13195128b49e15
Alfred Brännare <alfred-brannare@hotmail.com>
2025-11-25 17:10:43 +0100

    style: add Shadcn card design to navbar

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -1,26 +1,27 @@
-import { GoGoal } from "react-icons/go";
-import { HiOutlineUserCircle, HiPlusCircle } from "react-icons/hi";
+import {GoGoal} from "react-icons/go";
+import {HiOutlineUserCircle, HiPlusCircle} from "react-icons/hi";
+import { Card } from "../ui";
 
 export const NavigationBar = () => {
     return (
-        <nav className="fixed bottom-4 inset-x-4 md:inset-x-0 mx-auto max-w-3xl flex rounded-lg shadow
+        <nav>
+            <Card className="flex flex-row fixed bottom-4 inset-x-4 md:inset-x-0 mx-auto max-w-3xl flex rounded-lg shadow
                 bg-elevated-bg justify-evenly p-2 items-center">
+                <GoGoal
+                    size={32}
+                    className="text-amft-white"
+                />
 
-            <GoGoal
-            size={32}
-            className="text-amft-white"
-            />
+                <HiPlusCircle
+                    size={52}
+                    className="text-green"
+                />
 
-            <HiPlusCircle
-                size={52}
-                className="text-green"
-            />
-
-            <HiOutlineUserCircle
-                size={32}
-                className="text-amft-white"
-            />
-
+                <HiOutlineUserCircle
+                    size={32}
+                    className="text-amft-white"
+                />
+            </Card>
         </nav>
     )
 }
