799ffd649a774fedb99d48ce5930d32dc8355cf2
Alfred Brännare <alfred-brannare@hotmail.com>
2025-11-25 16:45:54 +0100

    style: update icons on navbar

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -1,28 +1,26 @@
-import {Button} from "@/components/ui/button";
-import {PlusCircle, Target, User} from "phosphor-react";
+import { GoGoal } from "react-icons/go";
+import { HiOutlineUserCircle, HiPlusCircle } from "react-icons/hi";
 
 export const NavigationBar = () => {
     return (
         <nav className="fixed bottom-4 inset-x-4 md:inset-x-0 mx-auto max-w-3xl flex rounded-lg shadow
-                bg-elevated-bg justify-evenly p-2">
+                bg-elevated-bg justify-evenly p-2 items-center">
 
-            <Target
-                size={52}
-                className="text-amft-white"
-                weight="duotone"
+            <GoGoal
+            size={32}
+            className="text-amft-white"
             />
 
-            <PlusCircle
-                weight="bold"
+            <HiPlusCircle
                 size={52}
                 className="text-green"
             />
 
-            <User
-                weight="duotone"
-                size={52}
+            <HiOutlineUserCircle
+                size={32}
                 className="text-amft-white"
             />
+
         </nav>
     )
 }
