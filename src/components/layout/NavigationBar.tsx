03a63c842c8218efcb8b396eaeced5c393aad3f3
Alfred Brännare <alfred-brannare@hotmail.com>
2026-01-03 12:48:59 +0100

    refactor: update icons to lucide-react

--- src/components/layout/NavigationBar.tsx
+++ src/components/layout/NavigationBar.tsx
@@ -1,25 +1,25 @@
-import {GoGoal} from "react-icons/go";
-import {HiOutlineUserCircle, HiPlusCircle} from "react-icons/hi";
 import { Card } from "../ui";
+import { UserRoundPen, Goal } from 'lucide-react';
 
 export const NavigationBar = () => {
     return (
         <nav>
             <Card className="flex flex-row fixed bottom-4 inset-x-2 md:inset-x-0 mx-auto max-w-3xl flex rounded-lg shadow
                 bg-elevated-bg justify-evenly p-2 items-center">
-                <GoGoal
-                    size={32}
-                    className="text-amft-white"
+                <Goal
+                    size={42}
+                    className="text-amft-white h-20 cursor-pointer transition-transform duration-300 hover:scale-110"
                 />
 
-                <HiPlusCircle
-                    size={52}
-                    className="text-green"
+                <img
+                    src="/AutomagicFoodTrackerLogo.png"
+                    alt="Register Meal Button"
+                    className="h-20 cursor-pointer transition-transform duration-300 hover:scale-110"
                 />
 
-                <HiOutlineUserCircle
-                    size={32}
-                    className="text-amft-white"
+                <UserRoundPen
+                    size={42}
+                    className="text-amft-white h-20 cursor-pointer transition-transform duration-300 hover:scale-110"
                 />
             </Card>
         </nav>
