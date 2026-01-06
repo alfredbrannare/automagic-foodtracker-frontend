66a9293f843160a5856d7bdd56b2ba1e4231bb04
Alfred Brännare <alfred-brannare@hotmail.com>
2026-01-06 11:53:11 +0100

    wip(style): tweak meal item layout

--- src/components/feature/meal/MealItem.tsx
+++ src/components/feature/meal/MealItem.tsx
@@ -15,29 +15,31 @@ export const MealItem = ({item, onRemove, onUpdate}: MealItemProps) => {
 
     return (
         <div>
-            <div className="flex flex-row justify-between">
-                <div>
-                    <Label className="text-md">{item.name}</Label>
+            <div className="flex flex-row justify-between items-center">
+                <div className="w-15 flex-shrink-0">
+                    <Label className="text-md truncate block">{item.name}</Label>
                     <span className="text-sm">{item.weight}g</span>
-                    <div className="flex justify-start items-center mt-2">
+                </div>
+                {!isExpanded &&
+                    <div className="flex-grow grid grid-cols-2 md:grid-cols-4 gap-x-4 items-center mt-2 mx-2">
                         {Object.entries(item.nutrition).map(([key, value]) => (
                             <div key={key} className="flex gap-1 mr-3 text-center items-center">
                                 <Progress
                                     value={100}
-                                    className="h-3 w-3 mr-1 rounded-sm bg-prog-bg z-0"
+                                    className="h-3 w-3 mr-1 rounded-sm bg-prog-bg z-0 max-[364px]:hidden"
                                     indicatorClassName="bg-green-500"
                                 />
-                                <span className="text-sm">{key}</span>
-                                <span>{value}g</span>
+                                <span className="text-[0.7rem]">{key}</span>
+                                <span className="text-[0.7rem]">{value}g</span>
                             </div>
                         ))}
                     </div>
-                </div>
+                }
                 <div
                     className={`flex items-center gap-2 transition-all duration-300 overflow-hidden ${isExpanded ? 'w-80 opacity-100' : 'w-0 opacity-0'}`}>
                     <h1>Hello</h1>
                 </div>
-                <button onClick={() => setIsExpanded(!isExpanded)}>
+                <button onClick={() => setIsExpanded(!isExpanded)} className="justify-end">
                     <GiHamburgerMenu
                         className="text-lines text-3xl cursor-pointer transition-transform duration-300 hover:scale-110"
                     />
