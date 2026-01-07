d30921321e9c97144400df99d3dcd2661c9527af
Alfred Brännare <alfred-brannare@hotmail.com>
2026-01-07 11:12:21 +0100

    wip: add message if empty meals array

--- src/components/feature/meal/MealSection.tsx
+++ src/components/feature/meal/MealSection.tsx
@@ -3,11 +3,11 @@ import {useMealContext} from "@/hooks/useMeal.ts";
 import {MealItem} from "@/components/feature/meal/MealItem.tsx";
 import {useNutritionContext} from "@/hooks/useNutrition.ts";
 import {useEffect} from "react";
-import { SkipBack, SkipForward } from 'lucide-react';
+import {SkipBack, SkipForward} from 'lucide-react';
 
 export const MealSection = () => {
-    const { mealItems, loading, error, removeItem, updateItem, refetch } = useMealContext();
-    const { selectedDate, changeDate } = useNutritionContext();
+    const {mealItems, loading, error, removeItem, updateItem, refetch} = useMealContext();
+    const {selectedDate, changeDate} = useNutritionContext();
 
     useEffect(() => {
         void refetch(selectedDate);
@@ -23,12 +23,15 @@ export const MealSection = () => {
     return (
         <Card className="bg-elevated-bg max-w-5xl w-full">
             <CardHeader className="flex flex-row justify-between items-center">
-                <button onClick={() => changeDate(-1)} className="hover:cursor-pointer transition-transform duration-300 hover:scale-110">
+                <button onClick={() => changeDate(-1)}
+                        className="hover:cursor-pointer transition-transform duration-300 hover:scale-110">
                     <SkipBack className="text-amft-white"/>
                 </button>
                 <CardTitle className="text-amft-white text-2xl">Meals</CardTitle>
-                <button onClick={() => changeDate(1)} className={`${isToday ? 'hover:cursor-not-allowed' : 'hover:cursor-pointer transition-transform duration-300 hover:scale-110'}`} disabled={isToday}>
-                    <SkipForward className={`text-amft-white ${isToday ? 'opacity-20' : ''}`} />
+                <button onClick={() => changeDate(1)}
+                        className={`${isToday ? 'hover:cursor-not-allowed' : 'hover:cursor-pointer transition-transform duration-300 hover:scale-110'}`}
+                        disabled={isToday}>
+                    <SkipForward className={`text-amft-white ${isToday ? 'opacity-20' : ''}`}/>
                 </button>
             </CardHeader>
             <CardContent className="flex flex-col justify-between">
@@ -39,17 +42,22 @@ export const MealSection = () => {
                     <LoadingContainer message="Loading storage items..."/>
                 ) : error ? (
                     <ErrorContainer title="Unable to fetch storage items" description={error}/>
+                ) : mealItems.length === 0 ? (
+                    <span className="text-center">
+                        You've not registered any meals {selectedDate.toISOString().split('T')[0]}
+                    </span>
                 ) : (
                     <>
                         {mealItems.map(item => (
                             <MealItem
-                            item={item}
-                            onRemove={removeItem}
-                            onUpdate={updateItem}
+                                item={item}
+                                onRemove={removeItem}
+                                onUpdate={updateItem}
                             />
                         ))}
                     </>
-                )}
+                )
+                }
             </CardContent>
         </Card>
     );
