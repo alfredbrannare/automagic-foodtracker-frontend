5b8c7fd571112d9439ad895d83016cd221cd00c8
Alfred Brännare <alfred-brannare@hotmail.com>
2026-01-07 16:05:22 +0100

    feat: add update user goals dialog and form validation

---
+++ src/components/feature/user/UpdateUserGoalsDialog.tsx
@@ -0,0 +1,124 @@
+import type {Goals, UpdateUserGoalsRequest, UserGoalsResponse} from "@/types/user";
+import {useState} from "react";
+import {useUserContext} from "@/hooks/useUser.ts";
+import {
+    Button,
+    Dialog, DialogClose,
+    DialogContent,
+    DialogDescription, DialogFooter,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger, ErrorInput, Input, Label
+} from "@/components/ui";
+import {UserRoundPen, Goal} from 'lucide-react';
+
+export const UpdateUserGoalsDialog = () => {
+    const {userGoals, updateGoals, loading} = useUserContext();
+
+    const [formData, setFormData] = useState<UpdateUserGoalsRequest>({
+        targetProtein: userGoals?.targetProtein ?? 0,
+        targetCarbs: userGoals?.targetCarbs ?? 0,
+        targetFat: userGoals?.targetFat ?? 0,
+        targetCalories: userGoals?.targetCalories ?? 0
+    });
+
+    const handleReset = () => {
+        setFormData({
+            targetProtein: userGoals?.targetProtein ?? 0,
+            targetCarbs: userGoals?.targetCarbs ?? 0,
+            targetFat: userGoals?.targetFat ?? 0,
+            targetCalories: userGoals?.targetCalories ?? 0
+        })
+    }
+
+    const isFormValid = formData.targetProtein >= 0 &&
+        formData.targetCarbs >= 0 &&
+        formData.targetCalories >= 0 &&
+        formData.targetFat >= 0;
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        await updateGoals(formData);
+    }
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>
+                <button>
+                    <Goal
+                        size={42}
+                        className="text-amft-white h-20 cursor-pointer transition-transform duration-300 hover:scale-110"
+                    />
+                </button>
+            </DialogTrigger>
+            <DialogContent className="sm:max-w-[425px]">
+                <form onSubmit={handleSubmit}>
+                    <DialogHeader className="mb-4">
+                        <DialogTitle>Update Goals</DialogTitle>
+                        <DialogDescription>
+                            This change can&apos;t be undone.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        {loading ? (
+                            <span>Loading...</span>
+                        ) : (
+                            <>
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="protein-1">Protein (g)</Label>
+                                        <Input id="protein-1" name="protein" type="number"
+                                               value={formData.targetProtein} onChange={(e) => setFormData({
+                                            ...formData,
+                                            targetProtein: Number(e.target.value)
+                                        })} required={true}/>
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="calories-1">Calories (g)</Label>
+                                        <Input id="calories-1" name="calories" type="number"
+                                               value={formData.targetCalories} onChange={(e) => setFormData({
+                                            ...formData,
+                                            targetCalories: Number(e.target.value)
+                                        })} required={true}/>
+                                    </div>
+                                </div>
+
+                                <div className="grid grid-cols-1 md:grid-cols-2 gap-3">
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="carbs-1">Carbs (g)</Label>
+                                        <Input id="carbs-1" name="carbs" type="number"
+                                               value={formData.targetCarbs} onChange={(e) => setFormData({
+                                            ...formData,
+                                            targetCarbs: Number(e.target.value)
+                                        })} required={true}/>
+                                    </div>
+                                    <div className="grid gap-3">
+                                        <Label htmlFor="fat-1">Fat (g)</Label>
+                                        <Input id="fat-1" name="fat" type="number" value={formData.targetFat}
+                                               onChange={(e) => setFormData({
+                                                   ...formData,
+                                                   targetFat: Number(e.target.value)
+                                               })} required={true}/>
+                                    </div>
+                                </div>
+                            </>
+                        )
+                        }
+
+                    </div>
+                    <DialogFooter className="mt-4">
+                        <DialogClose asChild>
+                            <Button type="button" variant="outline" onClick={handleReset}>Cancel</Button>
+                        </DialogClose>
+                        <DialogClose asChild>
+                            <Button type="submit" disabled={!isFormValid}>Save changes</Button>
+                        </DialogClose>
+                    </DialogFooter>
+                    <div className="grid gap-3 justify-center mt-2 text-center">
+                        {isFormValid ? null : <ErrorInput description="All fields must be positive numbers."/>}
+                    </div>
+                </form>
+            </DialogContent>
+        </Dialog>
+    )
+}
