f820ca39db9b77f5f8a49af8789fffa71ff3e35c
Alfred Brännare <alfred-brannare@hotmail.com>
2026-01-21 12:34:53 +0100

    style: minor design update to checking credentials when logging in

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { AuthProvider } from './context/AuthContext';
 import {useAuthContext} from './hooks/useAuth';
 import { Dashboard, Auth } from '@/pages';
+import {Spinner} from "@/components/ui";
 
 
 const AppContent: React.FC = () => {
     const { isAuthenticated, isLoading } = useAuthContext();
 
     if (isLoading) {
-        return <div>Checking credentials...</div>;
+        return (
+            <div className="flex flex-col justify-center items-center h-screen gap-2">
+                Checking credentials...
+                <Spinner />
+            </div>
+        )
     }
 
     if (!isAuthenticated) {
